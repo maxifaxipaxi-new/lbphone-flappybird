ed584dd7c047bc9b2fda1ab7b89cce77a9c9c579
MaxiFaxiPaxi <115405418+maxifaxipaxi-new@users.noreply.github.com>
2024-03-01 23:39:46 +0100

    Translated config.lua

--- mfp_lb-flappy/config.lua
+++ mfp_lb-flappy/config.lua
@@ -1,10 +1,10 @@
 Config = {}
 
 Config.name = "Flappy Börd" -- Name of the App
-Config.description = "Lasse deinen Vogel raus bei Flappy Börd." -- disc in the appstore (edit for your local)
+Config.description = "Set your bird free in Flappy Börd." -- disc in the appstore (edit for your local)
 Config.defaultApp = false -- OPTIONAL if set to true, app should be added without having to download it,
 Config.size = 1932 -- OPTIONAL in kb
-Config.price = 25 -- OPTIONAL, Make players pay with in-game money to download the app
+Config.price = 25 -- OPTIONAL, Make players pay with in-game money to download the app. Do 0 for it to be free.
 
 
 -- Support on discord.mfpscripts.com if needed. Check out issues on github. --
